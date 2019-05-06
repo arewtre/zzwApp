8757d8bd1d1dc7a8d20ef6e6ca21f59a2798290b
arewtre <33242627+arewtre@users.noreply.github.com>
2019-05-06 09:15:15 +0800

    Update config.js

--- common/config.js
+++ common/config.js
@@ -1,7 +1,7 @@
 //const ROOTPATH = "https://wx.diudanke.com/";
 
- const ROOTPATH =  'http://47.100.48.1/api/member/';
-//const ROOTPATH =  'http://www.chinapaper.net/api/member/';
+ const ROOTPATH =  'http://xxxx/api/member/';
+//const ROOTPATH =  'http://xxxx/api/member/';
 module.exports = {
 	//APIHOST:ROOTPATH+"getData.php?ac=",
 	APIHOST:ROOTPATH,
